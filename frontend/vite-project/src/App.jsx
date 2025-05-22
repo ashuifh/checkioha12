dc4bc9bc2711788e4d7a0921d052894a49a82fde
ashuifh <111656275+ashuifh@users.noreply.github.com>
2025-05-22 22:26:33 +0530

    frontend design
    jafnla

---
+++ frontend/vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className=" flex  gap-4 justify-center items-center ">
+      <div className="border-rounded flex p-5  m-50 border  justify-center rounded-2xl">photo</div>
+      <div className="border-rounded rounded-2xl border flex  p-5 m-50 justify-center">descpription</div>
+    </div>
+  );
+}
+export default App;
